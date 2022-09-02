2ce0cbdebb13b463438b07a478899deb7223bd19
Lucas Gabriel Alves <lucasgabrielalvespereira45@gemail.com>
2022-09-02 19:28:02 -0400

    logica

---
+++ exemplo 5 - incremento e decremento de variáveis e operadores.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	//n1=n1+1;
+	 
+//OU
+	//n1+=10;
+	
+//OU
+
+	//n1++;  
+	//- vai atribuir 1 no valor da variável 
+	
+	//n1--;
+	//vai subtrair 1 no valor da variável 
+	
+//isso vale para qualquer operação
+	//n1*=2;  n1=n1*2;
+	//n1/=2;  n1=n1/2;
+	
+	
+	int n1=10;
+	
+	//n1++;
+	
+	//cout << n1 << "\n\n";
+	
+//OU	
+	
+	cout << ++n1 << "\n\n";
+	//Quando adicionada aqui, o "+" ou "-" é adicionado antes
+	return 0;
+	
+	
+}
