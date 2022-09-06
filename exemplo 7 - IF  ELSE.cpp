7b6d4210a117a0571ab3d5f2c26d622dfff7d11a
Lucas Gabriel Alves <lucasgabrielalvespereira45@gemail.com>
2022-09-05 20:16:06 -0400

    logica

---
+++ exemplo 7 - IF  ELSE.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	/*
+	se (dia de sol) { vou ao clube 
+// 	se o teste logico for verdeiro
+  } se não { vou ao cinema
+//	se o teste for falso 
+  }
+	*/
+
+
+//	teste lógico
+//	>, <, >=, >=, ==, !=
+//  10 < 5 = false 
+//50 > 25 = true
+// 100 >= (25 * 2) = true
+// num > 0 = true 
+//num >= 110 = true
+
+	int x=10;
+	int y=50;
+	int z=1;
+	char opc='s';
+
+	if( x < y ) {
+		cout << "resultado verdadeiro \n\n";
+	} else {
+		cout << "resultado falso \n" ;
+	}
+
+	if(z >= 1) {
+		cout << "vou ao clube \n\n" ;
+	}else {
+		"vou ao cinema \n" ;}
+
+
+
+
+
+	return 0;
+
+}
+
+
+
+
