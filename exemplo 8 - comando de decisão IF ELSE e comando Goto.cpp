47b4ebf779b4c50ec10ec8bc3fefe373b0c5150a
Lucas Gabriel Alves <111927340+LucasG4briel@users.noreply.github.com>
2022-09-06 18:22:52 -0400

    Update exemplo 8 - comando de decisão IF ELSE e comando Goto.cpp

--- exemplo 8 - comando de decisão IF ELSE e comando Goto.cpp
+++ exemplo 8 - comando de decisão IF ELSE e comando Goto.cpp
@@ -1,3 +1,48 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+ 
+ int main() {
+ 	
+int x, y, z;
+char opc;
+
+	inicio:
+
+	system("cls");
+
+
+cout << "digite o valor da nota 1: " ;
+	cin >> x;
+	
+cout << "digite o valor da nota 2: ";
+ 	cin >> y;
+ 	
+	z=x+y;
+ 	
+ 	/* 
+	 >= 60   	  Aprovado
+	 >= 40 e <60  recuperação
+	 < 40    	  reprovado
+	 */
+ 	
+	if(z >= 60) {
+		cout << "\n Aluno aprovado\n";
+	}else if(z >= 40){
+		cout << "\n Aluno em recuperaçao \n";
+	}else{
+		cout <<"\n Aluno reprovado \n";
+	}
+ 	
+ 	cout << "\nDigitar outra notas?[s/n]: ";
+ 	cin >> opc;
+ 	
+ 	if(opc =='s' or opc== 'S'){
+ 		goto inicio;
+	 }
+	 
+	 
+	 
+return 0;
+ }
 
