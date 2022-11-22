a07e713689b6f88d16a94a4296d33d4de65c0375
LucasG4briel <lucasg4p@gmail.com>
2022-11-21 23:09:54 -0300

    logica

---
+++ break.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int n;
+	
+	n=0;
+	
+	while(n<400){
+		
+		cout << n << "\n";
+		n++;
+		
+	if(n == 90) {
+			break;
+		} 
+	}
+	
+	
+	
+}
+
+
+//o comando break serve para "intenrromper" alguma sequencia ou comando, seria +/- "PARE" ou um tipo de "limitdor" 
+
